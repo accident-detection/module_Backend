db1109eac2fcda721414febf6115be8e843b17b9
Andrei Zvonimir Crnkovic <crnkovic.a.z@gmail.com>
2015-02-01 01:54:01 +0100

    Removed obsolete tests. Should implement with another testing facility.

--- test/testLog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-    var=$(tail -n 1 /var/log/system.log)
-    log="{\"logmsg\": \"$var\"}"
-    echo "$log" | curl -H "Content-Type: application/json" -d @- http://localhost:5000/api/events
-    sleep 5
-done
