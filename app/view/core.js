1fb89b5dbe99a012f53f95905b3554e2c05371b6
Andrei Zvonimir Crnkovic <crnkovic.a.z@gmail.com>
2015-02-01 11:14:53 +0100

    Preparing the site

--- app/view/core.js
+++ app/view/core.js
@@ -26,3 +26,30 @@ function mainController($scope, $http) {
 			$location.url('/500.html');
 		});
 };
+
+function deviceController($scope, $http) {
+	$scope.formData = {};
+
+	$http.get('/api/device/') //inject device id from URL??
+		.success(function(data) {
+			$scope.events = data;
+
+			var locations = [];
+
+			angular.forEach(data, function(value, key) {
+				var location = {
+					lat: value.GPSlat,
+					log: value.GPSlog,
+					alt: value.GPSalt
+				};
+
+				this.push(location);
+			}, locations);
+
+			$scope.locations = locations;
+		})
+		.error(function(data) {
+			// redirect to 500.html
+			$location.url('/500.html');
+		});
+};
