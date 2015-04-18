e15d2ada37f96cb2f965bee761640a70a397b6fe
Andrei Zvonimir Crnkovic <crnkovic.a.z@gmail.com>
2015-04-18 11:32:06 +0200

    Added EventRepo interface and Mongo Repo

---
+++ EventRepo/IEventRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventLibrary;
+
+namespace EventRepo
+{
+    public interface IEventRepo
+    {
+        public bool Save(Event e);
+        public Event FindById(int id);
+        public List<Event> FindByDate(DateTime date);
+        public List<Event> FindByToken(string token);
+        public List<Event> FindAll();
+    }
+}
---
+++ EventRepo/MongoDBRepo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventRepo
+{
+    public class MongoDBRepo : IEventRepo
+    {
+        public bool Save(EventLibrary.Event e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EventLibrary.Event FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EventLibrary.Event> FindByDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EventLibrary.Event> FindByToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EventLibrary.Event> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
