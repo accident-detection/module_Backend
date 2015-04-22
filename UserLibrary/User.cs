8a7d52658cb6196dea86cde381d153dfccfa7237
Andrei Zvonimir Crnkovic <crnkovic.a.z@gmail.com>
2015-04-22 09:13:44 +0200

    Added a User model and included the reference

---
+++ UserLibrary/User.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLibrary
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Token { get; set; }
+
+        public User(string name, string token)
+        {
+            Name = name;
+            Token = token;
+        }
+    }
+}
