3129fd10efac0b7b875fa8184569b75204652e5e
Andrei Zvonimir Crnkovic <crnkovic.a.z@gmail.com>
2015-05-03 18:33:47 +0200

    Basic table listing the data

---
+++ module_Backend/Controllers/PanelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace module_Backend.Controllers
+{
+    public class PanelController : Controller
+    {
+        //
+        // GET: /Panel/
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Panel";
+
+            return View();
+        }
+	}
+}
---
+++ module_Backend/Scripts/core.js
@@ -0,0 +1,16 @@
+﻿(function () {
+
+    function mainController($scope, $http) {
+        $scope.formData = {};
+
+        $http.get('api/events').success(function (data) {
+            $scope.events = data;
+        })
+        .error(function (data) {
+            console.log("Error loading data from API.");
+        });
+    };
+
+    angular.module("events", [])
+        .controller("mainController", ["$scope", "$http", mainController]);
+})();
