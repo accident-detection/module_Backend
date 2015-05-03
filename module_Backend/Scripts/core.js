ecb30d0b07d75f45cd9db3a5f97b793d6fc2e93b
Andrei Zvonimir Crnkovic <crnkovic.a.z@gmail.com>
2015-05-03 18:45:52 +0200

    Basic interpretation of data added

--- module_Backend/Scripts/core.js
+++ module_Backend/Scripts/core.js
@@ -3,7 +3,16 @@
     function mainController($scope, $http) {
         $scope.formData = {};
 
-        $http.get('api/events').success(function (data) {
+        $http.get('http://iis.home.andreicek.eu/adEvents/api/events').success(function (data) {
+            data.forEach(function (event) {
+                if (event.AdCode == 205)
+                    event.AdCodeMessage = "Hit from back";
+                else if (event.AdCode == 206)
+                    event.AdCodeMessage = "Hit in front";
+                else if (event.AdCode == 207)
+                    event.AdCodeMessage = "Hit while surrounded";
+            });
+
             $scope.events = data;
         })
         .error(function (data) {
